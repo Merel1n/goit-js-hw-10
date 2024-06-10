b70e9b109e56059f0faa333781b3019adb1f7e79
Merel1n <ineskamit@gmail.com>
2024-06-10 12:15:56 +0000

    Deploying to gh-pages from @ Merel1n/goit-js-hw-10@170ff64ebaaffba1e631b0af54a7174e07d2d6c1 🚀

---
+++ commonHelpers2.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-3cfb730f.js";const e=document.querySelector(".form"),o=e.querySelector(".form input");e.querySelector(".form button");console.log(`this is input ${o.value}`);e.addEventListener("submit",t=>{t.preventDefault,t=o.value});
+//# sourceMappingURL=commonHelpers2.js.map
