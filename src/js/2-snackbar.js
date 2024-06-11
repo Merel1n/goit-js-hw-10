72e7dc93eb9fbbc52caca12da6016a9471c8db4b
Inna Matviienko <ineskamit@gmail.com>
2024-06-11 17:41:50 +0300

    update task-2

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,8 +1,53 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+import imageDefsUrl from '../img/symbol-defs.svg';
+import imageOkUrl from '../img/symbol-ok.svg';
 const formElem = document.querySelector('.form');
-const inputValue = formElem.querySelector('.form input');
-const bnt = formElem.querySelector('.form button');
-console.log(`this is input ${inputValue.value}`);
-formElem.addEventListener('submit', e => {
-  e.preventDefault;
-  e = inputValue.value;
-});
+
+formElem.addEventListener('submit', formSubmit);
+
+function formSubmit(event) {
+  event.preventDefault();
+
+  const delay = document.querySelector('input[name="delay"]').value;
+  const state = document.querySelector('input[name="state"]:checked').value;
+  const promise = createPromise(delay, state);
+  formElem.reset();
+}
+
+function createPromise(delay, state) {
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (state === 'fulfilled') {
+        resolve(
+          iziToast.success({
+            title: 'Fulfilled',
+            message: `promise in ${delay}ms`,
+            titleColor: '#FFFFFF',
+            messageColor: '#FFFFFF',
+            backgroundColor: '#59A10D',
+            iconUrl: imageOkUrl,
+            iconColor: '#FAFAFB',
+            imageWidth: 302,
+            position: 'topRight',
+          })
+        );
+      } else {
+        reject(
+          iziToast.error({
+            title: 'Rejected',
+            message: `promise in ${delay}ms`,
+            titleColor: '#FFFFFF',
+            messageColor: '#FFFFFF',
+            backgroundColor: '#ef4040',
+            iconUrl: imageDefsUrl,
+            iconColor: '#FAFAFB',
+            imageWidth: 302,
+            position: 'topRight',
+          })
+        );
+      }
+    }, delay);
+  });
+  return promise;
+}
